6c28dde682d8bc04ab6d9ff90e8ccf2a67c8b25d
filalex11 <filalexant@yandex.ru>
2020-11-18 19:17:49 +0300

    symbol check

--- Lab_4.c
+++ Lab_4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
 	Ввести строку. Вывести слова, в которых каждая буква входит не менее двух раз.
 */
@@ -15,26 +16,44 @@ void check (char* string, int start_word, int end_word) {
 		for (i = start_word; i <= end_word; i++) {
 			printf("%c", string[i]);
 		}
-		printf("%c\n", '|');
+		printf("%c\n", '\0');
+	}
+}
+
+char is_symbol (char* string, int i) {
+	char symbols[17] = {' ', ',', '.', ':', ';', '-', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int j = 0, count = 0;
+	for (j = 0; j < 17; j++) {
+		if (symbols[j] == string[i]) {
+			count++;
+		}
+	}
+	if (count != 0) {
+		return 'Y';
+	} else {
+		return 'N';
 	}
 }
 
 int main () {
-	int a = 0, i = 0;
-	printf("Input string:");
-	char string[100];
+	int a = 0, i = 0, length = 0;
+	printf("Input length of string:");
+	scanf("%d\n", &length);
+	char* string = malloc (length * sizeof(char));
+	printf("Words:\n");
 	while ((a = getchar()) != '\n') {
 		string[i] = a;
 		i++;
 	}
 	i = 0;
-	int start_word = 0, end_word = 0, first_word = 1;
-
+	int start_word = 0, end_word = 0;
+	char first_word = 'Y';
 	while (string[i] != 0) {
-		if (string[i] != ' ') {
-			if (first_word == 1) {
+		if (is_symbol(string, i) == 'N') {	
+			
+			if (first_word == 'Y') {
 				start_word = i;
-				first_word = 0;
+				first_word = 'N';
 			}
 			if (string[i + 1] == 0) {
 				end_word = i;
@@ -42,9 +61,9 @@ int main () {
 			}
 			i++;
 		} else {
-			if (string[i] == ' ') {
+			if (is_symbol(string, i) == 'Y') {
 				end_word = i - 1;
-				first_word = 1;
+				first_word = 'Y';
 				check (string, start_word, end_word);
 				start_word = i + 1;
 				i++;
