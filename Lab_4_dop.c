b899da97cf0c45580d9939d481a7d4442e0ff664
filalex11 <filalexant@yandex.ru>
2020-11-27 17:42:16 +0300

    rubbish symbols

---
+++ Lab_4_dop.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+	Ввести строку. Вывести слова, в которых каждая буква входит не менее двух раз.
+	Реализовать с помощью двумерного массива.
+*/
+void check (char *word) {
+	int i = 0, j = 0, count = 0;
+	for (i = 0; word[i + 1] != 0; i++) {
+		for (j = i + 1; word[j] != 0; j++) {
+			if (word[i] == word[j]) {
+				count++;
+			}
+		}
+	}
+	if (count > 0) {
+		for (i = 0; word[i] != 0; i++) {
+			printf("%c", word[i]);
+		}
+		printf("%c\n", '\0');
+	}
+	
+}
+
+int is_letter (char str) {
+	if (((str >= 'A') && (str <= 'Z')) || ((str >= 'a') && (str <= 'z'))) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int word_count (char *string) {
+	int i = 0, in_word = 0, count = 0;
+	while (string[i] != 0) {
+		if (is_letter(string[i]) == 1) {
+			in_word = 1;
+		} else {
+			if (in_word == 1) {
+				count++;
+				in_word = 0;
+			}
+		}
+		i++;
+	}
+	if (in_word == 1) {
+		count++;
+	}
+	return count;
+}
+
+int main () {
+	int a = 0, length = 0, i = 0, j = 0, k = 0;
+	printf("Input length of string [1;1000]:");
+	scanf("%d\n", &length);
+	char *string = malloc (length * sizeof(char));
+	printf("Words:\n");
+	while (((a = getchar()) != '\n') && (i < length)) {
+		string[i] = a;
+		i++;
+	}
+	int count = word_count(string);
+	i = 0;
+	char words[count][length];
+	int symbols = 0;
+	while (string[k] != 0) {
+		if (is_letter(string[k]) == 1) {
+			words[i][j] = string[k];
+			j++;
+			symbols = 0;
+		} else {
+			symbols++;
+			if (symbols == 1) {
+				i++;
+				j = 0;
+			}
+		}
+		k++;
+	}
+	i = 0;
+	j = 0;
+	while (i != count) {
+		check(words[i]);
+		i++;
+	}
+	return 0;
+}
